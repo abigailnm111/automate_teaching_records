069ef9bb4f3f5a76f315d55b540f89cd22adfacc
abigailnm111 <60757439+abigailnm111@users.noreply.github.com>
2021-07-22 21:34:30 -0700

    reads all items

--- write_teaching_record.py
+++ write_teaching_record.py
@@ -12,8 +12,8 @@ import docx2txt
 import re
 import os
 
-faculty_names= []
-doc= Document('Teaching Record Temp.docx')
+faculty_names= [***] 
+               
 
 
             #Search column I for name to get row 
@@ -34,7 +34,7 @@ class evaluationScores:
         self.name= faculty.split(',')
         self.last_name= self.name[0]
         
-        self.course_id_column=''
+        
         
         
     def save_scores(self, rundown, q):
@@ -45,7 +45,7 @@ class evaluationScores:
             if upper_name in row.value:
                 
                 r= row.row
-                self.course_id= rundown.cell(column=get_quarter_columns(rundown, "Subject Course Section"), row=r).value
+                self.course_id= rundown.cell(column=get_quarter_columns(rundown, "Subject Course Section"), row=r).value[:-6]
                 self.title= rundown.cell(column=get_quarter_columns(rundown, "Course Title"), row=r).value
                 self.enrollment= rundown.cell(column=get_quarter_columns(rundown, "Enrollment"), row=r).value
                 self.response= rundown.cell(column=get_quarter_columns(rundown, "Response Rate"), row=r).value
@@ -53,10 +53,11 @@ class evaluationScores:
                 self.crs_avg=rundown.cell(column=get_quarter_columns(rundown, "Crs AVG"), row=r).value
                 self.dept_ins_avg=rundown.cell(column=get_quarter_columns(rundown, "Dept Inst AVG"), row=r).value
                 self.dept_crs_avg=rundown.cell(column=get_quarter_columns(rundown, "Dept Crs AVG"), row=r).value
-                self.courses.append([self.course_id] )
-                              #self.title, self.enrollment, self.response, self.ins_avg, 
-                              #self.crs_avg, self.dept_ins_avg, self.dept_crs_avg]
+                self.courses.append([self.course_id,
+                              self.title, self.enrollment, self.response, self.ins_avg, 
+                              self.crs_avg, self.dept_ins_avg, self.dept_crs_avg])
             self.all_scores[q]= self.courses
+            
         return self.all_scores
         
 def get_quarter_columns(rundown, column_name):
@@ -84,38 +85,45 @@ def open_rundown_file(yq):
     rundown= rundown_report.worksheets[0]
     return rundown
 
-
+def write_teaching_record(faculty):
+     name=faculty.faculty
+     print(name)
+     doc= Document('Teaching Record Temp.docx')
+     doc= re.sub("<NAME>", name, doc)
+     doc.save(faculty.faculty+"Teaching Record.docx")
 
 def main():
     #template= docx2txt.process("Teaching Record Temp.docx")
     quarters= get_quarters_years()
     #iterate through each faculty member
+    all_fac=[]
     for name in faculty_names:
-        faculty=evaluationScores(name)
-        
+            faculty=evaluationScores(name)   
+            all_fac.append(faculty)
         
         #iterate through each quarter
-        for q in quarters:
-            #open rundown file per quarter
-            rundown=open_rundown_file(q)
-            if rundown!=None:
-                #faculty.get_quarter_columns(rundown)
-                for name in faculty_names:
-        
-                    faculty.save_scores(rundown,q)
-            
-        print(faculty.all_scores)
+            for q in quarters:
+                #open rundown file per quarter
+                rundown=open_rundown_file(q)
+                if rundown!=None:
+                    #faculty.get_quarter_columns(rundown)
+                    for name in all_fac:
+                        
+                        name.save_scores(rundown,q)
+                    
+                        write_teaching_record(name)
+                
+            print( faculty.last_name, faculty.all_scores)
 #File Name variations            
 #19S_ENGL_DeptRundown - A_Instruct, B_TA_Eval.xlsx            
 #19F_ENGL_DeptRundown_Instructor_- A_Instruct_- B_TA_Eval.xlsx   
 #20W_ENGL_DeptRundown_Instructor_- A_Instruct_- B_TA_Eval.xlsx   
 #20S_ENGL_DeptRundown_Instructor_- A_Instruct_- B_TA_Eval.xlsx  
 main()
-        #template= re.sub(name, "<NAME>", template)
-        # insert each quarter in table and course info if necessary
+      
+
     
-    #doc.save(name+"Teaching Record.docx")
+   
     
-file= "WEAVER, E.M." 
-last_name="Weaver"
+
 
